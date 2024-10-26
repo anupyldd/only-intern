8971a0b2ab1628bb620fd733e02dc93a678fd8e8
anupyldd <anupyldd@gmail.com>
2024-10-26 16:51:48 +0300

    w2 init

---
+++ w2/form_template.php
@@ -0,0 +1,53 @@
+ 
+<table border="0" cellpadding="1" cellspacing="1"> 
+  <tbody> 
+    <tr><td> <blockquote> 
+          <h2><?=$FORM->ShowFormTitle("")?></h2>
+         </blockquote> </td><td> 
+        <h5><?=$FORM->ShowFormDescription("")?></h5>
+       </td></tr>
+   </tbody>
+ </table>
+ 
+<div> 
+  <table border="0" cellpadding="1" cellspacing="1"> 
+    <tbody> 
+      <tr><td> 
+          <table border="0" cellpadding="1" cellspacing="1"> 
+            <tbody> 
+              <tr><td><?=$FORM->ShowInputCaption("SIMPLE_QUESTION_573","")?> 
+                  <br />
+                 <?=$FORM->ShowInput('SIMPLE_QUESTION_573')?></td><td><?=$FORM->ShowInputCaption("SIMPLE_QUESTION_506","")?> 
+                  <br />
+                 <?=$FORM->ShowInput('SIMPLE_QUESTION_506')?></td></tr>
+             
+              <tr><td><?=$FORM->ShowInputCaption("SIMPLE_QUESTION_926","")?> 
+                  <br />
+                 <?=$FORM->ShowInput('SIMPLE_QUESTION_926')?></td><td><?=$FORM->ShowInputCaption("SIMPLE_QUESTION_828","")?> 
+                  <br />
+                 <?=$FORM->ShowInput('SIMPLE_QUESTION_828')?></td></tr>
+             </tbody>
+           </table>
+         
+          <br />
+         </td><td><?=$FORM->ShowInputCaption("SIMPLE_QUESTION_204","")?> 
+          <br />
+         <?=$FORM->ShowInput('SIMPLE_QUESTION_204')?></td></tr>
+     </tbody>
+   </table>
+ </div>
+ 
+<div> 
+  <table border="0" cellpadding="1" cellspacing="1" align="center"> 
+    <tbody> 
+      <tr><td> 
+          <br />
+         </td><td><?=$FORM->ShowSubmitButton("","")?></td></tr>
+     </tbody>
+   </table>
+ 
+  <br />
+ 
+  <br />
+ </div>
+ 
