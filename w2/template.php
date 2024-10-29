3ba096261905697cf413340866e05431a31e3da0
Dmitry Anupyld <anupyldd@gmail.com>
2024-10-29 13:58:10 +0300

    Update template.php

--- w2/template.php
+++ w2/template.php
@@ -6,12 +6,12 @@
 <body>
 <div class="contact-form">
     <div class="contact-form__head">
-        <div class="contact-form__head-title">Связаться</div>
-        <div class="contact-form__head-text">Наши сотрудники помогут выполнить подбор услуги и&nbsp;расчет цены с&nbsp;учетом ваших требований</div>
+        <div class="contact-form__head-title"><?=$arResult["FORM_TITLE"]?></div>
+        <div class="contact-form__head-text"><?=$arResult["FORM_DESCRIPTION"]?></div>
     </div>
 
     <?php if ($arResult["isFormNote"] == "Y"): ?>
-        <p class="contact-form__success">Спасибо! Ваша заявка отправлена.</p>
+        <p class="contact-form__success"><?=$arResult["FORM_NOTE"]?></p>
     <?php else: ?>
         <?= $arResult["FORM_HEADER"] ?>
         <form class="contact-form__form" action="<?= POST_FORM_ACTION_URI ?>" method="POST">
@@ -32,7 +32,7 @@
             <div class="contact-form__bottom">
                 <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы подтверждаете, что ознакомлены, полностью согласны и принимаете условия &laquo;Согласия на обработку персональных данных&raquo;.</div>
                 <button class="form-button contact-form__bottom-button" type="submit" data-success="Отправлено" data-error="Ошибка отправки">
-                    <div class="form-button__title">Оставить заявку</div>
+                    <div class="form-button__title"><?=htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]);?></div>
                 </button>
             </div>
         </form>
