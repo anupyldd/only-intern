bdf57ef51b742b06435d0b07d93a5eb9b86a1f7e
anupyldd <anupyldd@gmail.com>
2024-11-02 19:47:19 +0300

    w3 init

---
+++ w3/bitrix/news.list/.default/template.php
@@ -0,0 +1,56 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<div class="article-list article-list">
+<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+	<?=$arResult["NAV_STRING"]?><br />
+<?endif;?>
+<?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+
+	<div class="article-card" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+			<div class="article-card__title" href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
+				<?= $arItem["NAME"] ?>
+			</div>
+		<?endif?>
+		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+			<span class="article-card__date">
+				<?= $arItem["DISPLAY_ACTIVE_FROM"]?>
+			</span>
+		<?endif?>
+		<div class="article-card__content">
+			<div class="article-card__image sticky">
+				<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="" data-object-fit="cover"/>
+			</div>
+			<div class="article-card__text">
+				<div class="block-content" data-anim="anim-3">
+					<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+						<div class="article-item__content">
+							<?= $arItem["PREVIEW_TEXT"] ?>
+						</div>
+					<?endif;?>
+				</div>
+				<a class="article-card__button" href="#">Назад к новостям</a></div>
+		</div>
+	</div>
+<?endforeach;?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+	<br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>
+</div>
